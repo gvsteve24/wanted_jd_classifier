d4b843790bdae033d32c583326adfeb08bc331f8
gvsteve24 <lvvvy07@gmail.com>
2021-07-23 00:32:28 +0900

    model update to bigram

--- api/inference.py
+++ api/inference.py
@@ -1,13 +1,13 @@
+from preprocess import preprocess
+
+from collections import Counter
+import pickle
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import SVC
-from preprocess import preprocess
-from collections import Counter
-import pickle
 from ckonlpy.tag import Twitter
 
 class CustomUnpickler(pickle.Unpickler):
-
     def find_class(self, module, name):
         try:
             return super().find_class(__name__, name)
@@ -23,27 +23,22 @@ def mytokenizer(raw_sentence):
             tokenized_sentence.append(word[0])           
     return tokenized_sentence
 
-t_bow = CustomUnpickler(open('./model/t_bow.pkl', 'rb')).load()
-svm_main = CustomUnpickler(open('./model/svm_main.pkl', 'rb')).load()
-svm_pos = CustomUnpickler(open('./model/svm_pos.pkl', 'rb')).load()
-svm_prefer = CustomUnpickler(open('./model/svm_prefer.pkl', 'rb')).load()
-svm_req = CustomUnpickler(open('./model/svm_req.pkl', 'rb')).load()
+t_bow = CustomUnpickler(open('./model/t_bow_bigram.pkl', 'rb')).load()
+svm_main = CustomUnpickler(open('./model/svm_main_bi.pkl', 'rb')).load()
+svm_pos = CustomUnpickler(open('./model/svm_pos_bi.pkl', 'rb')).load()
+svm_prefer = CustomUnpickler(open('./model/svm_prefer_bi.pkl', 'rb')).load()
+svm_req = CustomUnpickler(open('./model/svm_req_bi.pkl', 'rb')).load()
 
 def get_prediction(data):
-    print(data['preferred'])
     corpus_main = preprocess(data['position'])
     corpus_pos = preprocess(data['mainTask'])
     corpus_req = preprocess(data['requirements'])
     corpus_prefer = preprocess(data['preferred'])
 
-    print(corpus_prefer)
-
     unseen_main_tfidf = t_bow.transform(corpus_main)
     unseen_pos_tfidf = t_bow.transform(corpus_pos)
     unseen_prefer_tfidf = t_bow.transform(corpus_prefer)
     unseen_req_tfidf = t_bow.transform(corpus_req)
-
-    print(unseen_prefer_tfidf)
 
     pred_main = svm_main.predict(unseen_main_tfidf)
     pred_pos = svm_pos.predict(unseen_pos_tfidf)
@@ -56,5 +51,5 @@ def get_prediction(data):
 
     num2cat = {k: v for k, v in zip([0, 1, 2, 3], ['개발', '경영, 비즈니스', '디자인', '마케팅, 광고'])}
     res = num2cat[result[0][0]] if result else ""
-    print(res)
+
     return res
